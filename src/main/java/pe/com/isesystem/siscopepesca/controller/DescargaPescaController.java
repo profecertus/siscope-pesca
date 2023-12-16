0948af89d5287878c5610445f60dc447bc4f5bfa
SunFruits <gestionpbi@sunfruits.com.pe>
2023-12-15 22:25:11 -0500

    Creación del Dockerfile

--- src/main/java/pe/com/isesystem/siscopepesca/controller/DescargaPescaController.java
+++ src/main/java/pe/com/isesystem/siscopepesca/controller/DescargaPescaController.java
@@ -1,10 +1,13 @@
 package pe.com.isesystem.siscopepesca.controller;
 
+import com.mongodb.DBObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/descarga/v1")
@@ -16,12 +19,13 @@ public class DescargaPescaController {
     @PostMapping("/saveDescarga")
     public ResponseEntity<String> saveDescarga(@RequestBody Object descarga){
         mongoTemplate.save(descarga, "descarga-pesca");
-        System.out.println(descarga);
-        return new ResponseEntity<>("df", HttpStatus.OK);
+        return new ResponseEntity<>("OK ", HttpStatus.OK);
     }
 
-    @GetMapping("/saludo")
-    public String getSaludo() {
-        return "¡Hola, mundo!";
+    @GetMapping("/getDescargas")
+    public ResponseEntity<List<DBObject>> getDescargas() {
+        List<DBObject> documentos = mongoTemplate.findAll(DBObject.class, "descarga-pesca");
+        return new ResponseEntity<>(documentos, HttpStatus.OK);
+
     }
 }
